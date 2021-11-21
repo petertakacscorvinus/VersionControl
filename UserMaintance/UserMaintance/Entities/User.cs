c286d4dd596b7c2d5039a34627301ef720e4bad7
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-11-21 16:59:49 +0100

    first steps

---
+++ UserMaintance/UserMaintance/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintance.Entities
+{
+    internal class User
+    {
+        public Guid guid { get; set; } = Guid.NewGuid();
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string _fullname;
+
+        public string FullName
+        {
+            get { return LastName + " " + FirstName; }
+        }
+
+    }
+}
