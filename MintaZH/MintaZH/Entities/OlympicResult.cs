741abb0761920cf618317b2f8c5fbf0f25d3b5aa
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-11-26 17:13:18 +0100

    adatmodell + alapbetöltés

---
+++ MintaZH/MintaZH/Entities/OlympicResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MintaZH.Entities
+{
+    internal class OlympicResult
+    {
+        public int Year { get; set; }
+
+        public string Country { get; set; }
+
+        public int[] vs { get; set; }
+
+        public int Position { get; set; } 
+    }
+}
--- MintaZH/MintaZH/Form1.cs
+++ MintaZH/MintaZH/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using MintaZH.Entities;
 
 namespace MintaZH
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results = new List<OlympicResult>();
+        
         public Form1()
         {
             InitializeComponent();
+            Betolt("Summer_olympic_Medals.csv");
+        }
+
+        void Betolt (string fajlnev)
+        {
+            using (StreamReader sr = new StreamReader(fajlnev))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    string[] mezok = sor.Split(',');
+                    OlympicResult or = new OlympicResult();
+                    or.Year = int.Parse(mezok[0]);
+                    or.Country = mezok[3];
+                    int[] mtomb = new int[3];
+                    mtomb[0] = int.Parse(mezok[5]);
+                    mtomb[1] = int.Parse(mezok[6]);
+                    mtomb[2] = int.Parse(mezok[7]);
+                    results.Add(or);
+                }
+            }
+
+
         }
     }
 }
