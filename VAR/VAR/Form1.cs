044054b76df4256f1213691899fa34e8e9fcc97b
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-11-26 16:05:45 +0100

    Portfólió tárolása a memóriában

--- VAR/VAR/Form1.cs
+++ VAR/VAR/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VAR.Entities;
 
 namespace VAR
 {
@@ -14,6 +15,7 @@ namespace VAR
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         
         public Form1()
         {
@@ -21,6 +23,21 @@ namespace VAR
 
             ticks = context.Ticks.ToList();
             dataGridView1.DataSource = ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            PortfolioItem p = new PortfolioItem();
+            p.Index = "OTP";
+            p.Volume = 10;
+            Portfolio.Add(p);
+
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
