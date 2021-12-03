1d044fb163bd411a6495e4afe846d5fab36e6b69
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-12-03 16:33:19 +0100

    CarFactory osztály

---
+++ ProgramTervezesiMintak/ProgramTervezesiMintak/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using ProgramTervezesiMintak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTervezesiMintak.Entities
+{
+    public class CarFactory : IToyFactory
+    {
+        public  Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
