1280473dd39d17a8d2034ab4eb3db682a5cbc7bb
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-12-03 16:19:54 +0100

    Játék absztrakció kezelése

---
+++ ProgramTervezesiMintak/ProgramTervezesiMintak/Abstractions/Toy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramTervezesiMintak.Abstractions
+{
+    public abstract class Toy:Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Toy_Paint;
+
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public void MoveToy()
+        {
+            Left++;
+        }
+    }
+}
