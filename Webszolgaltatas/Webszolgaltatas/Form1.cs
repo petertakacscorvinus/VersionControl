b0db969d0b9e3744cb57a7b381ecfbe81da6ad01
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-12-09 22:15:31 +0100

    Webszolgáltatás hívása

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolgaltatas.MnbServiceReference;
 
 namespace Webszolgaltatas
 {
     public partial class Form1 : Form
     {
+        
+        
+        
         public Form1()
         {
             InitializeComponent();
+            getRates();
+        }
+
+        private void getRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody req = new GetExchangeRatesRequestBody();
+            req.currencyNames = "EUR";
+            req.startDate = "2020-01-01";
+            req.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(req);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
