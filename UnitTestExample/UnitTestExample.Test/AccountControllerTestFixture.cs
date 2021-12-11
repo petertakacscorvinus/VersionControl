25372a3ef9c5e05663edbe62ff69a0fd9f1f57f7
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-12-11 20:55:36 +0100

    Register függvény HappyPath

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -49,5 +49,24 @@ namespace UnitTestExample.Test
 
         }
 
+        [Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+
+            ]
+
+        public void TestRegisterHappyPath(string email, string password)
+        {
+
+            
+            var accountController = new AccountController();
+            
+            var actualResult = accountController.Register(email, password);
+            
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            //Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
     }
 }
