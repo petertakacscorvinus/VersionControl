05ccfb75d5c3b625ba851a9a4ee9225e1189b909
petertakacscorvinus <peter.takacs1@stud.uni-corvinus.hu>
2021-12-14 23:09:06 +0100

    Adatok betöltése

--- Mikroszimulacio/Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Mikroszimulacio.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,39 @@ namespace Mikroszimulacio
 {
     public partial class Form1 : Form
     {
+        List<Person> Population;
+        List<BirthProbabilities> BirthProbabilities;
+        List<DeathProbabilities> DeathProbabilities;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+
+            List<Person> result = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+
+                while (!sr.EndOfStream)
+                {
+
+                    string line = sr.ReadLine();
+                    string[] items = line.Split(';');
+                    Person p = new Person();
+                    p.BirthYear = int.Parse(items[0]);
+                    p.Gender = (Gender)Enum.Parse(typeof(Gender), items[1]);
+                    p.NbrOfChildren = int.Parse(items[2]);
+                    result.Add(p);
+                }
+            }
+            return result;
+
+        }
+
+
     }
 }
